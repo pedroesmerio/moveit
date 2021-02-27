20637c5a0300b2c50c68cbc92f3af73d681a7293
pedroesmerio <p.h.esmerio@gmail.com>
2021-02-27 13:17:49 -0300

    Aula 2 - Next | Home page style done

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,8 @@ class Document extends NextDocument {
     return (
       <Html>
         <Head>
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
+
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,30 @@
 import Head from 'next/head';
+
 import { ExperienceBar } from '../src/components/ExperienceBar.tsx';
+import { Profile } from '../src/components/Profile.tsx';
+import { CompletedChallenges } from '../src/components/CompletedChallenges.tsx';
+import { Countdown } from '../src/components/Countdown.tsx';
+
+import styles from '../src/styles/pages/Home.module.css';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <Head>
+        <title>Início | Moveit Next</title>
+      </Head>
+
       <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div>
+        </div>
+      </section>
     </div>
   );
 }
